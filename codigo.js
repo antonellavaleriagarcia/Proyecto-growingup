baff0af927bfba4f9e33b11ee12d99ac367ea047
antonellavaleriagarcia <antonellavaleriagarcia@gmail.com>
2022-04-28 14:41:03 -0300

    Libreria y otros avances

--- codigo.js
+++ codigo.js
@@ -1,12 +1,15 @@
 //CLIENTES
 
+
+
 class Persona {
-    constructor(nombre, apellido, edad, mail, telefono){
+    constructor(nombre, apellido, edad, mail, contrasenia, telefono){
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
         this.mail = mail;
-        this.mail = telefono;
+        this.contrasenia = contrasenia;
+        this.telefono = telefono;
     }
 }
 
@@ -14,21 +17,6 @@ const clientes = []
 
 const agregarClienteLista = (persona) => {
     clientes.push(persona)
-}
-
-const getNombre = document.getElementById("nombre");
-const getApellido = document.getElementById("apellido");
-const getEdad = document.getElementById("edad");
-const getMail = document.getElementById("mail");
-const getTelefono = document.getElementById("telefono");
-
-let formCliente = document.getElementById("formulario");
-formCliente.addEventListener("submit", crearCliente);
-
-function crearCliente(e){
-    e.preventDefault();
-    const cliente = new Persona(getNombre.value, getApellido.value, getEdad.value, getMail.value);
-    agregarClienteLista(cliente);
 }
 
 const cliente1 = new Persona("Maria", "Zucchi", 52, "marita@gmail.com");
@@ -43,14 +31,90 @@ const verListaClientes = () => {
 
 console.log(verListaClientes());
 
+const getNombre = document.getElementById("nombre");
+const getApellido = document.getElementById("apellido");
+const getEdad = document.getElementById("edad");
+const getMail = document.getElementById("mail");
+const getContrasenia = document.getElementById("contrasenia");
+const getTelefono = document.getElementById("telefono");
+const form = document.getElementById("formulario");
 
-function buscarPersona(nombre, clientes) {
-    let i = clientes.length - 1;
-    while (i >= 0 && nombre != clientes[i].nombre) {
-        i--;
-    }
-    return i;
+const crearCliente = (client) => {
+    clientes.push(client)
+    localStorage.setItem("clientes", JSON.stringify(clientes))
 }
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const nombre= getNombre.value
+    const apellido = getApellido.value
+    const edad = getEdad.value
+    const mail = getMail.value
+    const contrasenia = getContrasenia.value
+    const telefono = getTelefono.value
+
+    let cliente = new Persona(nombre, apellido, edad, mail, contrasenia, telefono);
+
+    crearCliente(cliente)
+    
+})
+
+
+const usuario = []
+
+let clientesJS = JSON.parse(localStorage.getItem('clientes')) || []
+clientesJS.push(JSON.parse(localStorage.getItem('clientes')) || []);
+
+class Usu {
+    constructor(mail, contraseña){
+        this.mail = mail;
+        this.contraseña = contraseña;
+       
+    }
+}
+ 
+
+
+function recordarUsuario(array){
+for (let i = 0; i < array.length; i++){
+    let {mail : usu} = array[i];
+    let {contrasenia : contra} = array[i];
+    aux = new Usu(usu, contra);
+    usuario.push(aux);
+    console.log(usuario) 
+    localStorage.setItem("usuario", JSON.stringify(usuario))
+}
+}
+recordarUsuario(clientesJS);
+
+
+
+const usuarioini = document.getElementById("Usuario");
+const contra = document.getElementById("contrasenia");
+
+
+
+
+let iniciar = document.getElementById("formularioIniciar");
+
+iniciar.addEventListener("submit", e => {
+    e.preventDefault()
+    const usuin= usuarioini.value
+    const contrain = contrase.value
+    
+    const recordar = new Usu(usuin, contrain);
+    guardar(recordar);
+})
+
+
+function guardar(item){
+    if(usuario.find(i => i.mail === item.mail)&&usuario.find(i => i.contraseña === item.contraseña)){
+        console.log(item);
+     }else{
+         console.log("ese usuario todavia no fue registrado")
+     }
+}
+
 /*
 let eliminar = buscarPersona(
     prompt("Ingrese el nombre del cliente que quiere eliminar"),
@@ -114,7 +178,7 @@ const totalPlata = document.getElementById("total");
 
     
     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-    carrito.push(JSON.parse(localStorage.getItem("carrito")) || [])
+    carrito.push(JSON.parse(localStorage.getItem("carrito")) || []);
     carrito.forEach((item) => {
         const fila = document.createElement("tr")
         fila.innerHTML= `<th>${item.nombre}</th><th>${item.precio}</th><th>${item.cantidad}</th>` 
@@ -187,12 +251,20 @@ function newItem(item){
            })
            console.log(carrito);
            listadoUpdate(carrito);
+           swal ({
+               title: "Añadio al carrito con exito",
+               icon: "success"
+           })
                     
         }else{
             item.cantidad=1;
             carrito.push(item);
             console.log(carrito);
             listadoUpdate(carrito);
+            swal ({
+                title: "Añadio al carrito con exito",
+                icon: "success"
+            })
         }
 }
   
@@ -286,8 +358,26 @@ function listadoUpdate(array) {
 }
 
 vaciar.onclick = (array) => {
-    carrito = [];
-    localStorage.setItem("carrito", JSON.stringify(array))
-    listadoUpdate(array);
+    swal ({
+        title: "Esta seguro que desea vaciar el carrito?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+    .then((willDelete) => {
+    if (willDelete) {
+        carrito = [];
+        localStorage.setItem("carrito", JSON.stringify(array))
+        listadoUpdate(array);  
+        swal("Su carrito ha sido vaciado", {
+        icon: "success",
+        });
+    } else {
+        swal("No fue elimado");
+    }
+    });
     
 };
+
+        
