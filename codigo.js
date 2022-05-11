10e7545901eb083af833b60f81a589fdaa874bc8
antonellavaleriagarcia <antonellavaleriagarcia@gmail.com>
2022-05-11 11:13:03 -0300

    Fin

--- codigo.js
+++ codigo.js
@@ -134,26 +134,7 @@ function guardar(item){
 
 const asd = document.getElementById("botoncerrar");
 
-if(usuario.length===0){
-    asd.onclick = () => {
-        swal ({
-            title: "Seguro que desea cerrar sesion?",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        })
-        .then((willDelete) => {
-        if (willDelete) {
-            usuario = []
-            localStorage.setItem("usuario", JSON.stringify(usuario))
-            boton.innerText = " ";
-            
-        } else {
-            swal("No fue elimado");
-        }
-    });
-    
-};}
+
 console.log(verListaClientes());
 
 //PRODUCTOS
@@ -183,16 +164,18 @@ const verListaProductos = () => {
 }
 
 //Creo productos
-const amazonia=new Producto ("Amazonia", 600, "img/amazonia.jpg", "Poetenciador Radicular, bio-estimulante orgánico que promueve la formación de raíces fuertes y sanas, tallos vigorosos. Mejora la asimilacion de nutrientes, mejora las condiciones del suelo, promueve la formación de micro-organismo beneficiosos para el suelo y para las plantas", "Fertilizantes", 6)
-const cultivante=new Producto("Cultivante", 2000, "img/coco.webp", "Sustrato Premium con el agregado óptimo de fibra de coco, que ofrece a las raíces un mejor acceso al aire y los nutrientes, para que crezcan más sanas y fuertes.", "Sustratos & Macetas", 4)
-const carpa=new Producto("Carpa", 1000, "img/carpa.png", "Especificaciones técnicas:• Carpa Probox suiza 100 x 100 x 200 cm.• Posee mylar 420D, 100% aprueba de luz.• Doble cierre reforzado.• Estructura sólida, varillas y base doble.• Tubos de refrigeración incorporados. • Caja cerrada.", "Indoor", 8)
-const maceta=new Producto ("Maceta", 950, "img/maceta.jpg", "Sistema de guias internas que evitan estrangulacion y genera una autopoda aerea de tus raices, logrando un desarrollo explosivo. Su exclusivo sistema de drenaje, no solo evita exceso de humedad, sino que te permite olvidarte de usar leca en tus plantas. Medidas Boca: 22 x 22cm Alto: 34 cm Base: 14 x 14cm", "Sustratos & Macetas", 10)
-const feeding=new Producto("Feeding", 1950, "img/feeding.jpg", "Alchimia os presenta la versión 125gr del abono Powder Feeding Hybrids de Green House, con ratio NPK de 15-7-22, idóneo para todo el ciclo de cultivo de las variedades de marihuana híbridos Indica - Sativa. Este abono mineral soluble asegura que nuestras plantas disponen de todos los nutrientes que necesitan para un crecimiento fuerte y una floración explosiva. Solo bastará diluir 0.5 hasta 2 gramos de Powder Feeding Hybrids por cada litro de agua, según la etapa del desarrollo de nuestras plantas. Características de G", "Sustratos & Macetas", 5)
-const cooler = new Producto("Cooler", 2600, "img/cooler.jpg","Descripción Turbina de 120x120mm con rodamiento.  Carcasa de aluminio con paletas plásticas. Salida con chicote de cable de aproximadamente 15 centímetros Soplador / Turbina / Cooler / FAN de 120mmx120mmx38mm Caracteristicas: Dimensiones: 120x120x25mm", "Indoor", 4)
-const balanza= new Producto("Balanza de pesaje", 1500, "img/balanza.jpg", "CARACTERISTICAS: UNIDADES DE PESAJE: Gramos / Onza / DWT. CAPACIDAD MAXIMA: 500 gramos. PRECISION: 0,1 gramo MEDIDAS: 12,00 cm. x 6,20 cm. x 2,00 cm. SISTEMA DE FRENO SIMPLE.", "Accesorios", 8)
-const estaca = new Producto("estaca", 450, "img/estaca.png", "Estaca de riego por goteo. La misma permite colocar cualquier botella de PVC común con pico de 1/2. Pueden ser botellas de 500 cc hasta 2 litros. Tiene regulador de caudal del riego por goteo, puede ser lento, normal, rápido y muy rápido. Muy fácil de usar e instalar. Ideal para macetas o huerta.", "Accesorios", 4)
-const amazing = new Producto("amazing", 2700, "img/amazing.png", "ADITIVO DE SILICIO PARA EL RENDIMIENTO VEGETAL ALIEN SKIN ofrece los beneficios del sílice otorgando a tus plantas resistencia al estrés y un aumento de la producción y el rendimiento. Benéfico en cultivos hidropónicos, coco o sustrato. Es un potenciador del crecimiento que pude ser usado junto con cualquier fertilizante base. El silcio es el segundo elemento más abundante de la corteza terrestre, su aporte proporciona troncos más fuertes, y una mejor captura de la luz, lo que se traduce en cogollos más densos, de mayor peso y tamaño.", "Fertilizantes", 5)
-const azteca= new Producto("azteca", 1650, "img/azteca.jpg", "Compuesto liquido a base de Bacillius y bacterias, formulado específicamente para potenciar el desarrollo radicular y crecimiento de las plantas. Producción explosiva de raíces, mayor resistencia a la sequía. Aumenta el rendimiento. Fija el nitrógeno atmosférico. Hace viable la disolución de fósforo y potasio.", "Fertilizantes", 3)
+const amazonia=new Producto ("AMAZONIA 150gr", 600, "img/amazonia.jpg", "Poetenciador Radicular, bio-estimulante orgánico que promueve la formación de raíces fuertes y sanas, tallos vigorosos. Mejora la asimilacion de nutrientes, mejora las condiciones del suelo, promueve la formación de micro-organismo beneficiosos para el suelo y para las plantas", "Fertilizantes", 6)
+const cultivante=new Producto("CULTIVATE COCO PREMIUM 25L", 2000, "img/coco.webp", "Sustrato Premium con el agregado óptimo de fibra de coco, que ofrece a las raíces un mejor acceso al aire y los nutrientes, para que crezcan más sanas y fuertes.", "Sustratos & Macetas", 4)
+const carpa=new Producto("CARPA SANTA PLANTA 60", 1000, "img/carpa.png", "Especificaciones técnicas:• Carpa Probox suiza 100 x 100 x 200 cm.• Posee mylar 420D, 100% aprueba de luz.• Doble cierre reforzado.• Estructura sólida, varillas y base doble.• Tubos de refrigeración incorporados. • Caja cerrada.", "Indoor", 8)
+const maceta=new Producto ("MACETA ROOTHOUSE - 18LT", 950, "img/maceta.jpg", "Sistema de guias internas que evitan estrangulacion y genera una autopoda aerea de tus raices, logrando un desarrollo explosivo. Su exclusivo sistema de drenaje, no solo evita exceso de humedad, sino que te permite olvidarte de usar leca en tus plantas. Medidas Boca: 22 x 22cm Alto: 34 cm Base: 14 x 14cm", "Sustratos & Macetas", 10)
+const feeding=new Producto("FEEDING GROW 125gr", 1950, "img/feeding.jpg", "Alchimia os presenta la versión 125gr del abono Powder Feeding Hybrids de Green House, con ratio NPK de 15-7-22, idóneo para todo el ciclo de cultivo de las variedades de marihuana híbridos Indica - Sativa. Este abono mineral soluble asegura que nuestras plantas disponen de todos los nutrientes que necesitan para un crecimiento fuerte y una floración explosiva. Solo bastará diluir 0.5 hasta 2 gramos de Powder Feeding Hybrids por cada litro de agua, según la etapa del desarrollo de nuestras plantas. Características de G", "Sustratos & Macetas", 5)
+const cooler = new Producto("COOLER", 2600, "img/cooler.jpg","Descripción Turbina de 120x120mm con rodamiento.  Carcasa de aluminio con paletas plásticas. Salida con chicote de cable de aproximadamente 15 centímetros Soplador / Turbina / Cooler / FAN de 120mmx120mmx38mm Caracteristicas: Dimensiones: 120x120x25mm", "Indoor", 4)
+const balanza= new Producto("BALANZA", 1500, "img/balanza.jpg", "CARACTERISTICAS: UNIDADES DE PESAJE: Gramos / Onza / DWT. CAPACIDAD MAXIMA: 500 gramos. PRECISION: 0,1 gramo MEDIDAS: 12,00 cm. x 6,20 cm. x 2,00 cm. SISTEMA DE FRENO SIMPLE.", "Accesorios", 8)
+const estaca = new Producto("ESTACA DE RIEGO", 450, "img/estaca.png", "Estaca de riego por goteo. La misma permite colocar cualquier botella de PVC común con pico de 1/2. Pueden ser botellas de 500 cc hasta 2 litros. Tiene regulador de caudal del riego por goteo, puede ser lento, normal, rápido y muy rápido. Muy fácil de usar e instalar. Ideal para macetas o huerta.", "Accesorios", 4)
+const amazing = new Producto("AMAZING NUTRIENTS ALIEN SKIN 1L", 2700, "img/amazing.png", "ADITIVO DE SILICIO PARA EL RENDIMIENTO VEGETAL ALIEN SKIN ofrece los beneficios del sílice otorgando a tus plantas resistencia al estrés y un aumento de la producción y el rendimiento. Benéfico en cultivos hidropónicos, coco o sustrato. Es un potenciador del crecimiento que pude ser usado junto con cualquier fertilizante base. El silcio es el segundo elemento más abundante de la corteza terrestre, su aporte proporciona troncos más fuertes, y una mejor captura de la luz, lo que se traduce en cogollos más densos, de mayor peso y tamaño.", "Fertilizantes", 5)
+const azteca= new Producto("AZTEKA BUDHA JUICE 250ml", 1650, "img/azteca.jpg", "Compuesto liquido a base de Bacillius y bacterias, formulado específicamente para potenciar el desarrollo radicular y crecimiento de las plantas. Producción explosiva de raíces, mayor resistencia a la sequía. Aumenta el rendimiento. Fija el nitrógeno atmosférico. Hace viable la disolución de fósforo y potasio.", "Fertilizantes", 3)
+const jabon= new Producto("JABÓN POTÁSICO CULTIVATE 250ml", 500, "img/jabon.png", "El jabón potásico es un insecticida y acaricida que combate eficazmente al pulgón, la cochinilla, la mosca blanca, los trips y la araña roja, entre otros; sin toxicidad para el ser humano y otros animales. Este plaguicida ecológico actúa por contacto; reblandeciendo la cutícula protectora de los parásitos, que utilizan además para respirar. Provoca la asfixia de éstos, sin dañar a otros insectos beneficiosos como las abejas o sus propios predadores naturales, útiles también para controlar la plaga. Este plaguicida no entra en la planta (no es sistémico) y se degrada rápidamente, por lo que no requiere plazo de seguridad. Además, tiene un efecto limpiador sobre las hojas: lava los residuos generados por los insectos, evitando la aparición de la negrilla. También es útil para combatir este hongo al inicio de la infección. Es biodegradable e inocuo. Para colmo, de rebote, es una fuente de potasio para las plantas. El mejor momento para realizar el tratamiento será a la salida o la puesta del sol, en ausencia de viento y de lluvia que lave la aplicación. Serán convenientes varios tratamientos para controlar la plaga. Lo ideal es repetirlos entre 3 y 4 veces, dejando una semana de descanso entre ellos.", "Plagas y hongos ", 8)
+const alambre= new Producto("ALAMBRE PLÁSTICO TUTOR 50m", 1450, "img/alambre.jpg", "Material: Plástico Color: verde Longitud: 50 metros Calidad superior, durable y útil Viene con un cortador práctico, solo presione para cortar la longitud que necesita Herramienta de jardinería multifuncional, puede usarlo para plantas fijas, bolsas de unión, líneas de punción, etc.", "Accesorios", 5)
 
 //Agrego productos al array
 agregarProductoLista(amazonia);
@@ -205,6 +188,8 @@ agregarProductoLista(balanza);
 agregarProductoLista(estaca);
 agregarProductoLista(amazing);
 agregarProductoLista(azteca);
+agregarProductoLista(jabon);
+agregarProductoLista(alambre);
 
 //Visualizo el array
 console.log(verListaProductos());
@@ -317,7 +302,7 @@ function agregarItemHtml(items){
         <img src="${item.imagen}" class="card-img-top altoimagen" alt="...">
         <div class="card-body card-tienda" >
             <h5 class="card-title" id="nombre">${item.nombre}</h5>
-            <p class="card-text" id="precio">${item.precio}</p>
+            <p class="card-text" id="precio">$ ${item.precio}</p>
             <div class="d-grid gap-1">
                 <button class="btn btn-outline" type="button" data-bs-toggle="collapse" data-bs-target="#colapsar${item.nombre}" aria-expanded="false" aria-controls="collapseExample">Ver más</button>
                 <button class="btn btn-primary" id="agregar${item.nombre}">Comprar</button>
@@ -354,8 +339,8 @@ function listadoUpdate(array) {
        resta.innerText = "-";
        
        const thh=document.createElement("th");
-       thh.append(suma)
        thh.append(resta)
+       thh.append(suma)
        fila.append(thh)
        tcuerpo.append(fila)
 
@@ -418,6 +403,22 @@ vaciar.onclick = () => {
         swal("No fue elimado");
     }
     });
-    
 };
+comprar.onclick = () => {
+    if(carrito.length>0){
+    swal ({
+        title: "Tu compra fue exitosa",
+        icon: "success"
+    })
+    carrito = [];
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+        listadoUpdate(carrito); 
+        totalPlata.innerText = "0";
+    }else{
+        swal ({
+            title: "Tu carrito esta vacio",
+            icon: "warning"
+        })
+    }
+}
 
